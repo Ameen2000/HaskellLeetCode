300acdb612e87ff7e673f236ac9aa92151d299f2
Ameen2000 <Ameen2000@users.noreply.github.com>
2023-05-29 22:11:48 +0100

    Made a function that inverts a binary tree

--- BinaryTrees.hs
+++ BinaryTrees.hs
@@ -1,12 +1,14 @@
 data Tree a =
     Leaf | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
 
+-- Find the depth of a Binary Tree
 depth :: Tree a -> Int
 depth Leaf = 0
 depth (Node x Leaf Leaf) = 1
 depth (Node x left right) =
     max (depth left + 1) (depth right + 1)
 
+-- Find the diameter of a binary tree
 dfs :: Tree a -> Int -> Int
 dfs Leaf i = i
 dfs (Node x left right) i =
@@ -21,3 +23,9 @@ treeDiameter (Node x Leaf Leaf) = 0
 treeDiameter (Node x left Leaf) = dfs (Node x left Leaf) (-1)
 treeDiameter (Node x Leaf right) = dfs (Node x Leaf right) (-1)
 treeDiameter (Node x left right) = dfs (Node x left right) 0
+
+-- Invert Binary Tree
+invert :: Tree a -> Tree a
+invert Leaf = Leaf
+invert (Node root Leaf Leaf) = Node root Leaf Leaf 
+invert (Node root left right) = Node root (invert right) (invert left)
